4c7fa9b47c098c483ac6edbbc6305de380982525
jacopomagnani2 <Jacopo@ADUAED07690LPMX.local>
2018-10-09 17:04:39 +0400

    updated practice rounds and instructions

--- Practice/models.py
+++ Practice/models.py
@@ -19,20 +19,21 @@ class Constants(BaseConstants):
     name_in_url = 'practice'
     players_per_group = None
     num_rounds = 2
-    game_space = [0, 1]
-    game_labels = ["A", "B"]
-    game_sequence = [0, 1]
+    game_sequence = [0, 4]
     type_space = [1, 2, 3]
     type_labels = ["X", "Y", "Z"]
-    A_match_value = [160, 80, 40]
-    A_reservation_value = [100, 75, 25]
-    B_match_value = [160, 80, 40]
-    B_reservation_value = [80, 75, 25]
+    status_space = [0, 1]
+    status_labels = ["active", "passive"]
+    match_value = [160, 80, 40]
+    reservation_value = [100, 75, 25]
     signal_space = [1, 2, 3]
     signal_names = ["red", "yellow", "blue"]
     pL = [0, 1/2, 1/2]
     pM = [0, 1, 0]
     pH = [1/2, 1/2, 0]
+    game_space = [0, 1, 2, 3, 4]
+    game_labels= ["A", "B", "C", "D", "E"]
+    prob_Haccept = [100, 75, 50, 25, 0]
 
 
 class Subsession(BaseSubsession):
@@ -54,6 +55,8 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     type = models.IntegerField()
+    status = models.IntegerField()
+    status_name = models.StringField()
     partner_type = models.IntegerField()
     signal = models.IntegerField()
     choice = models.IntegerField(
@@ -69,24 +72,47 @@ class Player(BasePlayer):
     points = models.IntegerField()
 
     def initialize(self):
-        # assign type, assign random partner's type and generate signal
+        # assign type, status, assign random partner's type and generate signal
         for t in range(1, Constants.num_rounds+1):
-            self.in_round(t).type = random.choice(Constants.type_space)
-            self.in_round(t).partner_type = random.choice(Constants.type_space)
-            if self.in_round(t).partner_type == 1:
-                self.in_round(t).signal = numpy.random.choice(Constants.signal_space, p=Constants.pH)
-            elif self.in_round(t).partner_type == 2:
-                self.in_round(t).signal = numpy.random.choice(Constants.signal_space, p=Constants.pM)
-            elif self.in_round(t).partner_type == 3:
-                self.in_round(t).signal = numpy.random.choice(Constants.signal_space, p=Constants.pL)
+            if t == 1:
+                self.in_round(t).type = 2
+                self.in_round(t).status = Constants.status_space[0]
+                self.in_round(t).status_name = Constants.status_labels[Constants.status_space.index(self.in_round(t).status)]
+                self.in_round(t).partner_type = random.choice(Constants.type_space)
+                if self.in_round(t).partner_type == 1:
+                    self.in_round(t).signal = numpy.random.choice(Constants.signal_space, p=Constants.pH)
+                elif self.in_round(t).partner_type == 2:
+                    self.in_round(t).signal = numpy.random.choice(Constants.signal_space, p=Constants.pM)
+                elif self.in_round(t).partner_type == 3:
+                    self.in_round(t).signal = numpy.random.choice(Constants.signal_space, p=Constants.pL)
+            elif t == 2:
+                self.in_round(t).type = random.choice(Constants.type_space)
+                self.in_round(t).status = Constants.status_space[1]
+                self.in_round(t).status_name = Constants.status_labels[
+                    Constants.status_space.index(self.in_round(t).status)]
+                self.in_round(t).partner_type = 2
+                if self.in_round(t).partner_type == 1:
+                    self.in_round(t).signal = numpy.random.choice(Constants.signal_space, p=Constants.pH)
+                elif self.in_round(t).partner_type == 2:
+                    self.in_round(t).signal = numpy.random.choice(Constants.signal_space, p=Constants.pM)
+                elif self.in_round(t).partner_type == 3:
+                    self.in_round(t).signal = numpy.random.choice(Constants.signal_space, p=Constants.pL)
 
     def get_outcome(self):
-        if self.subsession.game == 0:
-            match_value = Constants.A_match_value
-            reservation_value = Constants.A_reservation_value
-        elif self.subsession.game == 1:
-            match_value = Constants.B_match_value
-            reservation_value = Constants.B_reservation_value
-        self.partner_choice = random.choice([0, 1])
+        match_value = Constants.match_value
+        reservation_value = Constants.reservation_value
+        if self.status == 0:
+            if self.partner_type == 2 or self.partner_type == 3:
+                self.partner_choice = 1
+            else:
+                self.partner_choice = numpy.random.choice([0, 1], p=[1 - Constants.prob_Haccept[self.subsession.game] / 100,
+                                                          Constants.prob_Haccept[self.subsession.game] / 100])
+        elif self.status == 1:
+            self.partner_choice = random.choice([0, 1])
+            if self.type == 2 or self.type == 3:
+                self.choice = 1
+            else:
+                self.choice = numpy.random.choice([0, 1], p=[1 - Constants.prob_Haccept[self.subsession.game] / 100,
+                                                             Constants.prob_Haccept[self.subsession.game] / 100])
         self.match = self.choice * self.partner_choice
         self.points = self.match * match_value[self.partner_type-1] + (1 - self.match) * reservation_value[self.type-1]
--- Practice/pages.py
+++ Practice/pages.py
@@ -12,19 +12,38 @@ class Intro(Page):
         self.player.initialize()
 
 
-class Page1(Page):
+
+class Page1Active(Page):
 
     form_model = 'player'
     form_fields = ['choice']
+
+    def vars_for_template(self):
+        return {'prob': Constants.prob_Haccept[self.subsession.game]}
+
+    def is_displayed(self):
+        return self.player.status == 0
 
     def before_next_page(self):
         if self.timeout_happened:
             self.player.late = 1
             flip = random.randint(0,1)
             if flip ==0:
-                self.player.choice = 0
+                self.player.choice = False
             else:
-                self.player.choice = 1
+                self.player.choice = True
+        self.player.get_outcome()
+
+
+class Page1Passive(Page):
+
+    def vars_for_template(self):
+        return {'prob': Constants.prob_Haccept[self.subsession.game]}
+
+    def is_displayed(self):
+        return self.player.status == 1
+
+    def before_next_page(self):
         self.player.get_outcome()
 
 
@@ -39,7 +58,8 @@ class FinalPage(Page):
 
 page_sequence = [
     Intro,
-    Page1,
+    Page1Active,
+    Page1Passive,
     Page2,
     FinalPage
 ]
